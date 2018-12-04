5cc75f3f2b61c4fbc3234a3e8bee8a6edef76dc5
WareJosephB <warejosephb@gmail.com>
2018-12-04 16:21:35 +0000

    Added git script, fixed docker compose again

---
+++ gitscrape.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+repositories=(BabyNameGeneratorAPI BabyNameAPI BabyNameConsumer)
+baseRepo = https://github.com/WareJosephB
+
+git_clone() {
+        git_clone $1
+}
+for repository in ${repositories[@]}; do
+        git_clone ${baseRepo}/${repository}
+done
